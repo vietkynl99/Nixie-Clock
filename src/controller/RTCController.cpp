d744f247699f870c73a8d14efd13903d1e1f5d82
vietkynl99 <vietdohoang1999@gmail.com>
2023-12-12 16:07:17 +0700

    Edit valid date

--- src/controller/RTCController.cpp
+++ src/controller/RTCController.cpp
@@ -96,7 +96,20 @@ bool RTCController::isValidTime(int hour, int minute, int second)
 
 bool RTCController::isValidDate(int year, int month, int day)
 {
-    return year >= 2000 && year <= 2099 &&
-           month >= 1 && month <= 12 &&
-           day >= 1 && day <= 31;
+    if (year < 2000 || year > 2099 || month < 1 || month > 12 || day < 1 || day > 31)
+    {
+        return false;
+    }
+
+    int maxDays = 31;
+    if (month == 4 || month == 6 || month == 9 || month == 11)
+    {
+        maxDays = 30;
+    }
+    else if (month == 2)
+    {
+        maxDays = year % 4 == 0 ? 29 : 28;
+    }
+
+    return (day <= maxDays);
 }
